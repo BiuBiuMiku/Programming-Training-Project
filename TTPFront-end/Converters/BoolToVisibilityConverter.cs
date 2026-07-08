cf50ef81dd14a4924ddc612fa41ca71600d290e3
moranfanhua <moxiaosoffical@outlook.com>
2026-07-08 16:05:53 +0800

    update app&appConverter

--- TTPFront-end/Converters/BoolToVisibilityConverter.cs
+++ TTPFront-end/Converters/BoolToVisibilityConverter.cs
@@ -66,3 +66,18 @@ public class ZeroToVisibleConverter : IValueConverter
         throw new NotSupportedException();
     }
 }
+
+/// 两个值相等返回 true，用于 DataTrigger 判断是否选中
+public class EqualityConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.Length < 2) return false;
+        return Equals(values[0], values[1]);
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
